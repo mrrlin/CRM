3be526da341fed1f2739b8297cfdeec9417c8a38
Angelina <angelina.kulyaeva@yandex.ru>
2023-10-30 14:48:04 +0300

    Prompt and e=mc**2

---
+++ js/einstein.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const dogWeight = 20;
+const speedLight = 3e8;
+
+console.log(`Dog's body energy is ${dogWeight * speedLight ** 2}`);
--- js/script.js
+++ js/script.js
@@ -8,8 +8,23 @@ const price = 10000;
 const sum = price * quantity;
 
 console.log(`Товар: ${closet},
-количество: ${quantity},
-категория: ${category},
-цена: ${price}`);
+Количество: ${quantity},
+Категория: ${category},
+Цена: ${price}`);
 
 console.log(`Товар: ${closet}, общая сумма: ${sum}`);
+
+const userGoodName = prompt('Введите название товара: ');
+const userGoodQuantity = parseInt(prompt('Введите количество товара: '));
+const userGoodCategory = prompt('Введите категорию товара: ');
+const userGoodPrice = parseInt(prompt('Введите цену товара: '));
+
+const userGoodSumPrice = userGoodPrice * userGoodQuantity;
+
+console.log(`Товар: ${userGoodName},
+Количество: ${userGoodQuantity} шт.,
+Категория: ${userGoodSumPrice},
+Цена: ${userGoodPrice}`);
+
+console.log(`Товар: ${userGoodName} в количестве: ${userGoodQuantity}
+шт. и общей стоимостью: ${userGoodSumPrice} деревянных`);
