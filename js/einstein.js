2a02bfaa647403657cf916d73e97e2f1cc2d3498
Angelina <angelina.kulyaeva@yandex.ru>
2023-11-06 13:09:05 +0300

    Number input check

--- js/einstein.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const dogWeight = 20;
-const speedLight = 3e8;
-
-console.log(`Dog's body energy is ${dogWeight * speedLight ** 2}`);
--- js/script.js
+++ js/script.js
@@ -1,34 +1,24 @@
 'use strict';
 
-{
-  const furnitureName = 'Valery';
-  const quantity = 3;
-  const category = 'Closet';
-  const price = 10000;
-  
-  const sum = price * quantity;
-  
-  console.log(`Товар: ${furnitureName},
-  Количество: ${quantity},
-  Категория: ${category},
-  Цена: ${price}`);
-  
-  console.log(`Товар: ${furnitureName}, общая сумма: ${sum}`);
-}
+const furnitureName = prompt('Введите название товара: ');
+const quantity = +prompt('Введите количество товара: ');
+const category = prompt('Введите категорию товара: ');
+const price = +prompt('Введите цену товара: ');
 
-{
-  const furnitureName = prompt('Введите название товара: ');
-  const quantity = parseInt(prompt('Введите количество товара: '));
-  const category = prompt('Введите категорию товара: ');
-  const price = parseInt(prompt('Введите цену товара: '));
+let sum = 0;
+
+if (!parseInt(quantity) || !parseInt(price)) {
+  console.log('Вы ввели некорректные данные');
+} else {
+  sum = quantity * price;
   
-  const sum = quantity * price;
-  
-  console.log(`Товар: ${furnitureName},
+  console.log(`
+  Товар: ${furnitureName},
   Количество: ${quantity} шт.,
   Категория: ${category},
   Цена: ${price}`);
   
-  console.log(`Товар: ${furnitureName} в количестве: ${quantity}
-  шт. и общей стоимостью: ${price} деревянных`);
+  console.log(`
+  Товар: ${furnitureName} в количестве: ${quantity}
+  шт. и общей стоимостью: ${sum} деревянных`);
 }
