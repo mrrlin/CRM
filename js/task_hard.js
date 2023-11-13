17ebffb69b8bf74d9541f55fd18b5bc6e8e76494
Angelina <angelina.kulyaeva@yandex.ru>
2023-11-13 15:32:37 +0300

    Fourth task

---
+++ js/task_hard.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const allCashbox = [
+  [12, 4500], 
+  [7, 3210], 
+  [4, 650], 
+  [3, 1250], 
+  [9, 7830], 
+  [1, 990], 
+  [6, 13900], 
+  [1, 370]
+];
+
+const [totalCount, totalPrice] = allCashbox
+  .reduce(([totalCount, totalPrice], [count, price]) =>
+    [totalCount + count, totalPrice + price]);
+
+const averagePriceGoods = Math.round(totalPrice / totalCount);
+
+console.log(averagePriceGoods);
