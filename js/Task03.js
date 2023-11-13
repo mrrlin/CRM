f39af3c39d12170ba4b5ca7b75ad3bf3855fb6c1
Angelina <angelina.kulyaeva@yandex.ru>
2023-11-13 13:19:42 +0300

    Third task

--- js/Task03.js
+++ js/Task03.js
@@ -1,35 +1,16 @@
 'use strict';
 
-const calculate = (totalAmount, goodsCount, promoCode) => {
-  let currentTotalAmount = totalAmount;
-  const currentGoodsCount = goodsCount;
-  const currentPromocode = promoCode;
+const reverseString = (userString) => {
+  let currentUserString = userString;
+  let resultString = '';
 
-  if (currentGoodsCount > 10) {
-    currentTotalAmount -= currentTotalAmount * 0.03;
+  for (let i = 0; i < currentUserString.length; i++) {
+    resultString += currentUserString[(currentUserString.length - 1) - i];
   }
 
-  if (currentTotalAmount > 30000) {
-    currentTotalAmount -= (currentTotalAmount - 30000) * 0.15;
-  }
-
-  if (currentPromocode === 'METHED') {
-    currentTotalAmount -= currentTotalAmount * 0.1;
-  }
-  
-  if (currentPromocode === 'G3H2Z1' && currentTotalAmount > 2000) {
-    currentTotalAmount -= 500;
-  }
-
-  return currentTotalAmount.toFixed(2);
-};
-
-const userTotalAmount = +prompt('Введите общую сумму корзины');
-const usergoodsCount = +prompt('Введите количество товаров в корзине');
-const userpromoCode = prompt('Введите промокод');
-
-if (isNaN(userTotalAmount) || isNaN(usergoodsCount)) {
-  console.log('Вы ввели некорректные данные');
-} else {
-  console.log(`Стоимость корзины товаров после применения всех акций и скидок: ${calculate(userTotalAmount, usergoodsCount, userpromoCode)}`);
+  return resultString;
 }
+
+const userString = prompt('Введите строку');
+
+console.log(`${userString} => ${reverseString(userString)}`);
