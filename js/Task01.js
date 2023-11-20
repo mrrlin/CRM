9ba5732cb5db46269242f431f9ec543c355f547f
Angelina <angelina.kulyaeva@yandex.ru>
2023-11-20 13:03:56 +0300

    Generate array

--- js/Task01.js
+++ js/Task01.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const generateArray = (lengthArray) => {
+  const mas = [];
+  
+  for(let i = 0; i < lengthArray; i++) {
+    mas[i] = Math.floor((Math.random() * 100), 0);;
+  }
+
+  return mas;
+};
+
+console.log(generateArray(10));
