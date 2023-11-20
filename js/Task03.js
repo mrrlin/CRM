8c16fbff89aca12980e5112da2a938542b95d4bd
Angelina <angelina.kulyaeva@yandex.ru>
2023-11-20 14:12:53 +0300

    Generate array with limits and parity

--- js/Task03.js
+++ js/Task03.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const generateArray = (lengthArray, n, m, parity) => {
+  const mas = [];
+
+  const min = Math.min(n, m);
+  const max = Math.max(n, m);
+  
+  while(mas.length < lengthArray) {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+
+    if (parity === 'odd' && randomNumber % 2 || parity === 'even' && !(randomNumber % 2)) {
+      mas.push(randomNumber);
+    }
+  }
+
+  return mas;
+};
+
+console.log(generateArray(100, -5, 11, 'even'));
