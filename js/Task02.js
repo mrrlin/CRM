6a53aa79e4484690a63b5a30e63b409a37a76408
Angelina <angelina.kulyaeva@yandex.ru>
2023-11-29 15:03:02 +0300

    Generate number

--- js/Task02.js
+++ js/Task02.js
@@ -1,5 +1,10 @@
-import {listIPv4} from './ipv4.js';
+'use strict';
 
-const getCountOfUnicIPv4Address = list => new Set(listIPv4).size;
+const generateNumber = (mas) => {
+  mas.push(Math.round(Math.random() * 10) + 1);
+  const sum = mas.reduce((acc, num) => acc += num, 0);
 
-getCountOfUnicIPv4Address(['qwe', 'asd', 'zxc']);
+  return sum < 50 ? generateNumber(mas) : mas;
+};
+
+console.log(generateNumber([]));
