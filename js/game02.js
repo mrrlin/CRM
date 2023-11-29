9f4de61f6339559230b39580a57a0f8effcf3f7b
Angelina <angelina.kulyaeva@yandex.ru>
2023-11-29 14:23:20 +0300

    Fixes

--- js/game02.js
+++ js/game02.js
@@ -1,67 +1,90 @@
 'use strict';
 
-const isNumber = function (n) {
+const isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 const inputMinMaxNumber = () => {
   let min = prompt('Enter the low limit');
-    if (!isNumber(min)) {
-      return inputMinMaxNumber();
-    }
-  let max = prompt('Enter the high limit');
-    if(!isNumber(max)) {
-      return inputMinMaxNumber();
-    }
-  return [min, max];
-}
-
-const startGame = () => {
-  const [userLowLimit, userHighLimit] = inputMinMaxNumber();
-  const num = Math.floor((Math.random() * (userHighLimit - userLowLimit + 1)) + userLowLimit);
-  const attempts = Math.floor((userHighLimit - userLowLimit) * 0.3);
-
-  const playGame = (attempt, num) => {
-
-    console.log(`attempt: ${attempt}, number: ${num}`);
-
-    if (attempt < 1) {
-      alert('Attempts left and game over');
-      return;
-    }
-
-    const userNumer = prompt('Enter number:');
-
-    if(userNumer === null) {
-      alert('Bye');
-      return;
-    } 
-    if(!isNumber(userNumer)) {
-      alert('Only numbers!!!');
-      playGame(attempt, num);
-    } 
-    if (userNumer === num) {
-      alert('Gracia, you won!!!');
-      return;
-    }
-    if (userNumer > num) {
-      attempt--;
-      alert(`The number is less, attempt left: ${attempt}`);
-      playGame(attempt, num);
-    }
-    if (userNumer < num) {
-      attempt--;
-      alert(`The number is greater, attempt left: ${attempt}`);
-      playGame(attempt, num);
-    }
-    
+  if (min === null) {
+    alert('Bye');
+    return;
   }
 
-  playGame(attempts, num);
+  if (!isNumber(min)) {
+    return inputMinMaxNumber();
+  }
 
-}
+  let max = prompt('Enter the high limit');
+  if (max === null) {
+    alert('Bye');
+    return;
+  }
 
-startGame();
+  if (!isNumber(max)) {
+    return inputMinMaxNumber();
+  }
+
+  min = Math.min(+min, +max);
+  max = Math.max(+min, +max);
+
+  return [min, max];
+};
+
+const [userLowLimit, userHighLimit] = inputMinMaxNumber();
+
+const num = Math.floor((Math.random() *
+  (userHighLimit - userLowLimit + 1)) + userLowLimit);
+let attempts = Math.floor((userHighLimit - userLowLimit) * 0.3);
+
+const masOfUserAttempts = [];
+
+const playGame = () => {
+  if (attempts < 1) {
+    alert('Attempts left and game over');
+    return;
+  }
+
+  const userNumber = prompt(`Enter number. You have ${attempts} tries.`);
+
+  if (userNumber === null) {
+    alert('Bye');
+    return;
+  }
+
+  if (+userNumber === num) {
+    alert('You won!');
+    return;
+  }
+
+  switch (true) {
+    case isNaN(userNumber) || userNumber === 0:
+      alert('Enter number');
+      break;
+    case masOfUserAttempts.includes(userNumber):
+      alert(`You'he alredy enter this number.
+        You've ${attempts} ${attempts > 1 ? 'attempts' : 'attempt'}`);
+      break;
+    case userNumber < num:
+      --attempts;
+      alert(`Bigger. You have ${attempts} tries left`);
+      masOfUserAttempts.push(userNumber);
+      alert(masOfUserAttempts);
+      break;
+    case userNumber > num:
+      --attempts;
+      alert(`Bigger. You have ${attempts} tries left`);
+      masOfUserAttempts.push(userNumber);
+      alert(masOfUserAttempts);
+      break;
+    case +userNumber === num:
+      alert('Right');
+  }
+
+  return playGame();
+};
+
+playGame();
 
 // let userNumber = 0;
 // let userLowLimit = 0;
